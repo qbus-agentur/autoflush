a09ac8c4ea804d4e702d142b1a8914207927e842
Benjamin Franzke <bfr@qbus.de>
2016-01-01 19:42:22 +0100

    Support automatic cache clearing for levelmedia: -1, slide
    Clear all subpages of the page were page.media changes: headerimage
    
    Were changes means:
     - sys_file_references are added/deleted
     - any property of sys_file_references changes
    
    Note: This assumes all pages use 'levelmedia: -1, slide' to render pages images.
    
    TODO: Try to make this configurable or at least opt-in.

---
+++ Classes/Hooks/LevelMediaFlush.php
@@ -0,0 +1,127 @@
+<?php
+namespace Qbus\Qbcache\Hooks;
+
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * LevelMediaFlush
+ *
+ * @author Benjamin Franzke <bfr@qbus.de>
+ * @package qbcache
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class LevelMediaFlush implements \TYPO3\CMS\Core\SingletonInterface
+{
+    /**
+     * @var array
+     */
+    protected $pageIdsToFlushRecursively = array();
+
+    /**
+     * Hook executed after the DataHandler performed one database operation
+     *
+     * @param  string      $status
+     * @param  string      $table
+     * @param  int|string  $id
+     * @param  array       $fieldArray
+     * @param  DataHandler $dataHandler
+     * @return void
+     */
+    public function processDatamap_afterDatabaseOperations(
+        $status,
+        $table,
+        $id,
+        &$fieldArray,
+        DataHandler $dataHandler
+    ) {
+
+        /* pages.media changes when a new image is added or an existing is removed */
+        if ($table === 'pages' && $status === 'update' && isset($fieldArray['media'])) {
+            $this->pageIdsToFlushRecursively[] = $id;
+        }
+
+        /**
+         * We consider a file_reference to be changed when a tstamp is set in the fieldArray.
+         *
+         * Note that tstamp is also set when a media file collapsible is opened, so we may clear to much.
+         * But thats better than to less.
+         */
+        if ($table === 'sys_file_reference' && $status === 'update' && isset($fieldArray['tstamp'])) {
+            /*
+            if ($status === 'new') {
+                $id = $dataHandler->substNEWwithIDs[$id];
+            }
+            */
+
+            $fileReference = $this->getDatabaseConnection()->exec_SELECTgetSingleRow(
+                'uid_foreign',
+                'sys_file_reference',
+                "tablenames = 'pages' AND fieldname = 'media' and table_local = 'sys_file' and uid = " . intval($id)
+            );
+
+            if ($fileReference) {
+                $this->pageIdsToFlushRecursively[] = intval($fileReference['uid_foreign']);
+            }
+        }
+    }
+
+    /**
+     * processDatamap_afterAllOperations
+     *
+     * @param DataHandler $dataHandler
+     */
+    public function processDatamap_afterAllOperations(DataHandler $dataHandler)
+    {
+        $cumulatedPageIds = array();
+
+        if (empty($this->pageIdsToFlushRecursively)) {
+            return;
+        }
+
+        $queryGenerator = $this->createQueryGenerator();
+        $cacheManager = $this->getCacheManager();
+
+        $this->pageIdsToFlushRecursively = array_unique($this->pageIdsToFlushRecursively);
+        foreach ($this->pageIdsToFlushRecursively as $pageId) {
+            $begin = 0;
+            $depth = 1000;
+            $perms_clause = '1';
+            $treeList = $queryGenerator->getTreeList($pageId, $depth, $begin, $perms_clause);
+
+            $ids = GeneralUtility::trimExplode(',', $treeList, true);
+            foreach ($ids as $id) {
+                $cumulatedPageIds[] = $id;
+            }
+        }
+
+        $cumulatedPageIds = array_unique($cumulatedPageIds);
+        foreach ($cumulatedPageIds as $pageId) {
+            $cacheManager->flushCachesInGroupByTag('pages', 'pageId_' . $pageId);
+        }
+    }
+
+    /**
+     * @return \TYPO3\CMS\Core\Database\QueryGenerator
+     */
+    protected function createQueryGenerator()
+    {
+        return GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\QueryGenerator::class);
+    }
+
+    /**
+     * @return \TYPO3\CMS\Core\Cache\CacheManager;
+     */
+    protected function getCacheManager()
+    {
+        return GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class);
+    }
+
+    /**
+     * @return \TYPO3\CMS\Core\Database\DatabaseConnection
+     */
+    protected function getDatabaseConnection()
+    {
+        return $GLOBALS['TYPO3_DB'];
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,5 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \Qbus\Qbcache\Hooks\LevelMediaFlush::class;
