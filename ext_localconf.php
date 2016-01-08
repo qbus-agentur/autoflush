1ab5ab1d38bb2021e55fd8cc6de87b00207d00c8
Benjamin Franzke <bfr@qbus.de>
2016-01-08 06:36:15 +0100

    MenuFlush: Directly hook into HMENU to add pageCacheTags
    We can use the filterMenuPages hook to link into HMENU
    without having to define IProcFunc in TypoScript.
    
    This commit removes the previously added RegisterMenu typoscript helper
    since this is all done automatically now.

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,10 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \Qbus\Autoflush\Hooks\LevelMediaFlush::class;
 
+/* Collect menu tags when rendering */
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages'][] =  \Qbus\Autoflush\Hooks\Frontend\RegisterMenuTags::class;
+
+/* Flush menu tags when editing */
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \Qbus\Autoflush\Hooks\MenuFlush::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] =  \Qbus\Autoflush\Hooks\MenuFlush::class;
 
