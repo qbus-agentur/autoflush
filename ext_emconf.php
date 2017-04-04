5cff8753590ab2472e0989142cb2c2c940bf2014
Benjamin Franzke <bfr@qbus.de>
2017-04-04 07:08:23 +0200

    Make as compatible with TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.3.3',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.6.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(
         ),
